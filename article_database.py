baa6ecb09d9121b1695c968a1ff4a43f22e038de
heleledd <157613131+heleledd@users.noreply.github.com>
2024-02-14 16:02:24 +0000

    wrote the sql to initialise the article database :))

--- article_database.py
+++ article_database.py
@@ -1,20 +1,46 @@
 import sqlite3
-from models import Article
+from models.article import Article
 
 
 class ArticleDatabase:
     def __init__(self):
         # CONNECT TO DATABASE "articles.db" using SQLITE3
-
+        con = sqlite3.connect("articles.db")
+        cur = con.cursor()
+        
         # create table "articles" if it does not exist
         # id (auto generate), title, abstract, url
-
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS articles(
+            id int AUTO_INCREMENT PRIMARY KEY, 
+            title varchar UNIQUE NOT NULL, 
+            abstract varchar NOT NULL, 
+            url varchar NOT NULL
+            )
+            """)
+        
         # create table "article_tags" if it does not exist
         # id (auto generate), article_id, tag_name
-
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS article_tags(
+            id int AUTO_INCREMENT PRIMARY KEY, 
+            article_id int NOT NULL, 
+            tag_name varchar NOT NULL,
+            FOREIGN KEY(article_id) REFERENCES articles(id)
+            )
+            """)
+        
         # create table "article_authors" if it does not exist
         # id (auto generate), article_id, author_name
-
+        cur.execute("""
+        CREATE TABLE IF NOT EXISTS article_authors (
+            id int AUTO_INCREMENT PRIMARY KEY, 
+            article_id int NOT NULL, 
+            author_name varchar NOT NULL, 
+            FOREIGN KEY(article_id) REFERENCES articles(id))
+            """)
+        
+        con.close()
         pass
 
     def add_article(self, article):
